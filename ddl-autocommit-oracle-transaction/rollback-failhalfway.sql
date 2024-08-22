d90e1c7041e609b47927f5c4982cf2b340d8e534
Stephen Atwell <78044713+stephenatwell@users.noreply.github.com>
2024-08-22 14:25:04 -0700

    Update rollback-failhalfway.sql

--- ddl-autocommit-oracle-transaction/rollback-failhalfway.sql
+++ ddl-autocommit-oracle-transaction/rollback-failhalfway.sql
@@ -1,7 +1,2 @@
-EXECUTE IMMEDIATE 'DROP TABLE ' || animal;
-EXCEPTION
-   WHEN OTHERS THEN
-      IF SQLCODE != -942 THEN
-         RAISE;
-      END IF;
--- should also revent the animals table to its old state, except that is never needed because that's the sql that failed to run.
+drop table animal
+-- should also revert the animals table to its old state, except that is never needed because that's the sql that failed to run.
